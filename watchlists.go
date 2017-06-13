a714704f606bf9b99c3d9b5d85ba7662c738e6b3
Andrew Stuart <andrew.stuart2@gmail.com>
2017-06-13 09:11:46 -0700

    Add watchlist docs, get actual Instruments

--- watchlists.go
+++ watchlists.go
@@ -2,10 +2,11 @@ package robinhood
 
 import (
 	"encoding/json"
-	"io"
-	"os"
+	"sync"
 )
 
+// A Watchlist is a list of stock Instruments that an investor is tracking in
+// his Robinhood portfolio/app.
 type Watchlist struct {
 	Name string `json:"name"`
 	URL  string `json:"url"`
@@ -14,6 +15,7 @@ type Watchlist struct {
 	Client *Client `json:",ignore"`
 }
 
+// Watchlists retrieves the watchlists for a given set of credentials/accounts.
 func (c *Client) Watchlists() ([]Watchlist, error) {
 	res, err := c.Get(epBase + "watchlists/")
 	if err != nil {
@@ -31,6 +33,7 @@ func (c *Client) Watchlists() ([]Watchlist, error) {
 	return r.Results, err
 }
 
+// GetInstruments returns the list of Instruments associated with a Watchlist.
 func (w *Watchlist) GetInstruments() ([]Instrument, error) {
 	res, err := w.Client.Get(w.URL)
 	if err != nil {
@@ -38,7 +41,53 @@ func (w *Watchlist) GetInstruments() ([]Instrument, error) {
 	}
 	defer res.Body.Close()
 
-	var r struct{ Results []Instrument }
-	err = json.NewDecoder(io.TeeReader(res.Body, os.Stderr)).Decode(&r)
-	return r.Results, err
+	var r struct {
+		Results []struct {
+			Instrument, URL string
+		}
+	}
+
+	err = json.NewDecoder(res.Body).Decode(&r)
+	if err != nil {
+		return nil, err
+	}
+
+	insts := make([]Instrument, len(r.Results))
+	wg := &sync.WaitGroup{}
+	wg.Add(len(r.Results))
+
+	for i := range r.Results {
+		go func(i int) {
+			defer wg.Done()
+
+			url := r.Results[i].Instrument
+			res, err := w.Client.Get(url)
+			if err != nil {
+				return
+			}
+			defer res.Body.Close()
+
+			var inst Instrument
+			err = json.NewDecoder(res.Body).Decode(&inst)
+			if err != nil {
+				return
+			}
+
+			insts[i] = inst
+		}(i)
+	}
+
+	wg.Wait()
+
+	// Filter slice for empties (if error)
+	retInsts := []Instrument{}
+	for _, inst := range insts {
+		if (inst != Instrument{}) {
+		}
+		{
+			retInsts = append(retInsts, inst)
+		}
+	}
+
+	return retInsts, err
 }
