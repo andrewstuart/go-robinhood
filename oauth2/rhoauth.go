fd3736238ff2f3aa0cb022d52ba354663f4d3d35
Andrew Stuart <andrew.stuart2@gmail.com>
2018-09-27 21:55:08 -0700

    OAuth2 impl

---
+++ oauth2/rhoauth.go
@@ -0,0 +1,70 @@
+package rhoauth
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/url"
+	"strings"
+	"time"
+
+	robinhood "astuart.co/go-robinhood"
+	"github.com/pkg/errors"
+	"golang.org/x/oauth2"
+)
+
+// DefaultClientID is used by the website.
+const DefaultClientID = "c82SH0WZOsabOXGP2sxqcj34FxkvfnWRZBKlBjFS"
+
+// Config is the type that contains u/p string
+type PasswordToken struct {
+	ClientID, Username, Password string
+}
+
+func (p *PasswordToken) Token() (*oauth2.Token, error) {
+	v := url.Values{
+		"username": []string{p.Username},
+		"password": []string{p.Password},
+	}
+
+	cli := p.ClientID
+	if cli == "" {
+		cli = DefaultClientID
+	}
+
+	u, _ := url.Parse(robinhood.EPLogin)
+	q := u.Query()
+	q.Add("client_id", cli)
+	q.Add("grant_type", "password")
+	q.Add("scope", "internal")
+	u.RawQuery = q.Encode()
+
+	req, err := http.NewRequest(
+		"POST",
+		u.String(),
+		strings.NewReader(v.Encode()),
+	)
+	if err != nil {
+		return nil, errors.Wrap(err, "could not create request")
+	}
+
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Set("Accept", "application/json")
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, errors.Wrap(err, "could not post login")
+	}
+	defer res.Body.Close()
+
+	var o struct {
+		oauth2.Token
+		ExpiresIn int `json:"expires_in"`
+	}
+	err = json.NewDecoder(res.Body).Decode(&o)
+	if err != nil {
+		return nil, errors.Wrap(err, "could not decode token")
+	}
+
+	o.Token.Expiry = time.Now().Add(time.Duration(o.ExpiresIn) * time.Second)
+
+	return &o.Token, nil
+}
---
+++ oauth2/rhoauth_test.go
@@ -0,0 +1,23 @@
+package rhoauth
+
+import (
+	"os"
+	"testing"
+
+	"github.com/davecgh/go-spew/spew"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestOauthPassword(t *testing.T) {
+	asrt := assert.New(t)
+
+	o := PasswordToken{
+		Username: os.Getenv("ROBINHOOD_USERNAME"),
+		Password: os.Getenv("ROBINHOOD_PASSWORD"),
+	}
+
+	tok, err := o.Token()
+	asrt.NoError(err)
+	asrt.NotNil(tok)
+	spew.Dump(tok)
+}
