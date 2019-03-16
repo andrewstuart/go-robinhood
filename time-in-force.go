94155f16ebf98645e42beb0f97e7ff335f569320
Richard the Lionheart <richardthelionheart@gmx.com>
2019-03-16 17:52:59 -0500

    Add FOK and docs for time in force

--- time-in-force.go
+++ time-in-force.go
@@ -16,8 +16,14 @@ func (t TimeInForce) MarshalJSON() ([]byte, error) {
 //go:generate stringer -type=TimeInForce
 // Well-known values for TimeInForce
 const (
+	// GTC means Good 'Til Cancelled.
 	GTC TimeInForce = iota
+	// GFD means Good For Day.
 	GFD
+	// IOC means Immediate Or Cancel.
 	IOC
+	// OPG means Opening (of market).
 	OPG
+	// FOK means Fill Or Kill.
+	FOK
 )
