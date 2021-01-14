5d0de2961fbec8114f8c8f2d7cc9329d5f2dfd2c
David Holtz <david.richard.holtz@gmail.com>
2021-01-14 11:51:19 -0500

    update instrument to have current fields from API

--- instrument.go
+++ instrument.go
@@ -5,24 +5,30 @@ import "fmt"
 // Instrument is a type to represent the "instrument" API type in the
 // unofficial robinhood API.
 type Instrument struct {
-	BloombergUnique    string      `json:"bloomberg_unique"`
-	Country            string      `json:"country"`
-	DayTradeRatio      string      `json:"day_trade_ratio"`
-	Fundamentals       string      `json:"fundamentals"`
-	ID                 string      `json:"id"`
-	ListDate           string      `json:"list_date"`
-	MaintenanceRatio   string      `json:"maintenance_ratio"`
-	MarginInitialRatio string      `json:"margin_initial_ratio"`
-	Market             string      `json:"market"`
-	MinTickSize        interface{} `json:"min_tick_size"`
-	Name               string      `json:"name"`
-	Quote              string      `json:"quote"`
-	SimpleName         interface{} `json:"simple_name"`
-	Splits             string      `json:"splits"`
-	State              string      `json:"state"`
-	Symbol             string      `json:"symbol"`
-	Tradeable          bool        `json:"tradeable"`
-	URL                string      `json:"url"`
+	BloombergUnique       string      `json:"bloomberg_unique"`
+	Country               string      `json:"country"`
+	DayTradeRatio         string      `json:"day_trade_ratio"`
+	DefaultCollarFraction string      `json:"default_collar_fraction"`
+	FractionalTradability string      `json:"fractional_tradability"`
+	Fundamentals          string      `json:"fundamentals"`
+	ID                    string      `json:"id"`
+	ListDate              string      `json:"list_date"`
+	MaintenanceRatio      string      `json:"maintenance_ratio"`
+	MarginInitialRatio    string      `json:"margin_initial_ratio"`
+	Market                string      `json:"market"`
+	MinTickSize           interface{} `json:"min_tick_size"`
+	Name                  string      `json:"name"`
+	Quote                 string      `json:"quote"`
+	RhsTradability        string      `json:"rhs_tradability"`
+	SimpleName            interface{} `json:"simple_name"`
+	Splits                string      `json:"splits"`
+	State                 string      `json:"state"`
+	Symbol                string      `json:"symbol"`
+	Tradeable             bool        `json:"tradeable"`
+	Tradability           string      `json:"tradability"`
+	TradableChainID       string      `json:"tradable_chain_id"`
+	Type                  string      `json:"type"`
+	URL                   string      `json:"url"`
 
 	c Client
 }
